d4e90fc5d0d39e105de6a52203e23945c455e487
Alfonso Manuel González Sígler <almagosicode@gmail.com>
2023-07-27 15:48:23 +0200

    v0.1 comando /count definido

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from telegram import Update
+from telegram import Update, CallbackQuery
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 
 token = "6591688320:AAEBPbOC9yZjQAhg7QYBOAdeduh7rgb4Q6U"
@@ -32,6 +32,15 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text="Ni puta idea de lo que dices Hulio."
     )
 
+async def count(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text= "EPA"
+    )
+    print("____________________")
+    print(CallbackQuery.message.chat_id)
+    print("____________________")
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
 
@@ -40,9 +49,12 @@ if __name__ == '__main__':
     caps_handler = CommandHandler('caps',caps)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
+    count_handler = CommandHandler('count',count)
+
     application.add_handler(start_handler)
     #application.add_handler(echo_handler)
     #application.add_handler(caps_handler)
+    application.add_handler(count_handler)
     application.add_handler(unknown_handler) # siempre el último en ser añadido
 
     application.run_polling()
