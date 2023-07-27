a5cebccd3e08aeb6fecafd39dce109b1d5139a05
Alfonso Manuel González Sígler <almagosicode@gmail.com>
2023-07-27 20:03:04 +0200

    v0.5 añadido metodo poop, lee los emoji de mierda

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from telegram import Update, CallbackQuery
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 
 token = "6591688320:AAEBPbOC9yZjQAhg7QYBOAdeduh7rgb4Q6U"
+poopEmoji = '\U0001F4A9'
+
 
 logging.basicConfig(
     format='%(acstime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+"""
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -26,34 +30,44 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         text=text_caps)
     
+
+"""
+
+async def poop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if(update.message.text == poopEmoji):
+        await context.bot.send_message(
+            chat_id= update.effective_chat.id,
+            text= "DEJA DE CAGAR"
+        )
+
+async def count(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text= poopEmoji
+    )
+
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Ni puta idea de lo que dices Hulio."
     )
 
-async def count(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text= "EPA"
-    )
-    print("____________________")
-    print(CallbackQuery.message.chat_id)
-    print("____________________")
-
 if __name__ == '__main__':
     application = ApplicationBuilder().token(token).build()
 
-    start_handler = CommandHandler('start',start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler('caps',caps)
+    #start_handler = CommandHandler('start',start)
+    #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    #caps_handler = CommandHandler('caps',caps)
+
+    poop_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), poop)
+    count_handler = CommandHandler('count',count)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
-    count_handler = CommandHandler('count',count)
-
-    application.add_handler(start_handler)
+    #application.add_handler(start_handler)
     #application.add_handler(echo_handler)
     #application.add_handler(caps_handler)
+
+    application.add_handler(poop_handler)
     application.add_handler(count_handler)
     application.add_handler(unknown_handler) # siempre el último en ser añadido
 
